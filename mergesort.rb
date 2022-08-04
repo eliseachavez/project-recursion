4a0130a26573fda391abafb3bc572ab9b0839967
lemur <noctiphobic@gmail.com>
2022-08-04 14:15:55 -0500

    Write merge method

--- mergesort.rb
+++ mergesort.rb
@@ -1,14 +1,29 @@
-def msort(list)
-  if list.size < 2
-    return
-  else
-    # sort the left half
-    l = msort(list.slice!(0,list.size/2))
-    # sort the right half
-    r = msort(list)
-    # merge the two sides
-    merge(l, r)
+def merge(l, r)
+  sorted = []
+  until l.size.zero? || r.size.zero?
+    if l[0] < r[0]
+      sorted.push(l[0])
+      l.shift
+    else
+      sorted.push(r[0])
+      r.shift
+    end
   end
+  sorted.push(l) unless l.size.zero?
+  sorted.push(r) unless r.size.zero?
+
 end
+
+def msort(list)
+  return list if list.size < 2
+
+  # sort the left half
+  l = msort(list.slice!(0, list.size/2))
+  # sort the right half
+  r = msort(list)
+  # merge the two sides
+  merge(l, r)
+end
+
 
 msort([1,5,3,6,8,3,2,8,1,3])
